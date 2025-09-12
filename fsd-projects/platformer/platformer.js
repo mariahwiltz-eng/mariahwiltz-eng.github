9f21c14db51e2cb966c15d3abdbf04d445d8ea94
mariahwiltz-eng <mariah.wiltz@jeffersonrise.org>
2025-09-12 16:40:53 +0000

    add portfolio page to website

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,12 +31,22 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-
+createPlatform(500, 0, 700, 410, "black");
+createPlatform(1300, 500, 600, 50, "red");
+createPlatform(420, 600, 880, 50, "black");
+createPlatform(1100, 360, 200, 50, "yellow");
+createPlatform(1300, 220, 200, 50, "lime");
+createPlatform(20, 540, 400, 50, "violet");
+createPlatform(100, 400, 200, 50, "blue");
+createPlatform(200, 250, 300, 50, "orange");
+createPlatform(10,  100, 300, 50, "red");
 
 
 
     // TODO 3 - Create Collectables
-
+createCollectable("diamond", 1350, 50);
+createCollectable("diamond", 10, 50, 0.5, 0.7);
+createCollectable("diamond", 200, 150, 0.5, 0.7);
 
 
     
